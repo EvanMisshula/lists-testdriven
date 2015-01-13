e5bdef3338242ba141baf4a98a66ef48ccf3a102
smythp <patricksmyth01@gmail.com>
2015-01-12 19:26:31 -0500

    First unit test

--- listsapp/tests.py
+++ listsapp/tests.py
@@ -1,5 +1,16 @@
 from django.test import TestCase
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+# class SmokeTest(TestCase):
+#     def test_bad_maths(self):
+#         self.assertEqual(1 + 1, 3)
+
+
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from listsapp.views import home_page #1
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  #2
+        self.assertEqual(found.func, home_page)  #3
